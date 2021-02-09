d500b96fbf3c84daa80548af174ad4b505ea18b3
Bogey <jeffreysbogart@gmail.com>
2021-02-08 18:03:36 -0800

    Setup frontend

---
+++ frontend/jefflix.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+document.addEventListener("DOMContentLoaded", () => {
+  const root = document.getElementById("root");
+  ReactDOM.render(<h1>Welcome to JEFFLIX</h1>, root);
+});
