06993a5719f31d587e7c8d80ff7c747d7c7f611e
Bogey <jeffreysbogart@gmail.com>
2021-02-09 16:15:08 -0800

    Add action types to session actions

--- frontend/jefflix.jsx
+++ frontend/jefflix.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { login, logout, signup } from './util/session_api_util';
 
 document.addEventListener("DOMContentLoaded", () => {
+
+  window.login = login; //remove after testing
+  window.logout = logout; //remove after testing
+  window.signup = signup; //remove after testing
+
   const root = document.getElementById("root");
   ReactDOM.render(<h1>Welcome to JEFFLIX</h1>, root);
 });
