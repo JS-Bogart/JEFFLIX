c2b42b5f0fea145f00e6dfdcff1359704c4ffe8e
Bogey <jeffreysbogart@gmail.com>
2021-02-09 21:34:48 -0800

    Add root reducer

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -11,8 +11,7 @@ const sessionReducer = (state = _nullSession, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_CURRENT_USER:
-      const currentUser = action.currentUser
-      return Object.assign({}, { currentUser });
+      return Object.assign({}, { currentUser: action.currentUser });
     case LOGOUT_CURRENT_USER:
       return _nullSession;
     default:
