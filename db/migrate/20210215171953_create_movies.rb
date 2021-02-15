19b67ca75cfdcae18231d78286f60adbe9ec1c29
Bogey <jeffreysbogart@gmail.com>
2021-02-15 09:29:28 -0800

    Add movies model, migration, and controller

---
+++ db/migrate/20210215171953_create_movies.rb
@@ -0,0 +1,11 @@
+class CreateMovies < ActiveRecord::Migration[5.2]
+  def change
+    create_table :movies do |t|
+      t.string :title, null: false
+      t.integer :year, null: false
+      t.text :synopsis, null: false
+      t.timestamps
+    end
+    add_index :movies, :title
+  end
+end
