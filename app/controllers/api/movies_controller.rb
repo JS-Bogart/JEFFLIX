742fc8feb6af827f86ca3600a372d9c9538c2ffb
Bogey <jeffreysbogart@gmail.com>
2021-02-15 10:19:28 -0800

    Add genre model and controller, and seed database with demo user

--- app/controllers/api/movies_controller.rb
+++ app/controllers/api/movies_controller.rb
@@ -1,2 +1,25 @@
 class Api::MoviesController < ApplicationController
+
+  def index
+    @movies = Movies.all
+    render "api/movies/index"
+  end
+
+  def show
+    @movie = Movies.find_by(id: params[:id])
+    render "api/movies/show"
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.save
+    else
+      render json: @movie.errors.full_messages, status: 401
+    end
+  end
+
+  private
+  def movie_params
+    params.require(:movie).permit(:title, :year, :synopsis)
+  end
 end
