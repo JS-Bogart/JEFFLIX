08d6944a39609084d956d240bee19fe309c0ebca
Bogey <jeffreysbogart@gmail.com>
2021-02-16 17:17:24 -0800

    Add movies reducer

--- frontend/actions/movie_actions.js
+++ frontend/actions/movie_actions.js
@@ -1,12 +1,22 @@
 import * as APIUtil from '../util/movies_api_util';
 
-// export const RECEIVE_ALL_MOVIES = "RECEIVE_ALL_MOVIES";
+export const RECEIVE_ALL_MOVIES = "RECEIVE_ALL_MOVIES";
 export const RECEIVE_MOVIE = "RECEIVE_MOVIE";
+
+const receiveAllMovies = movies => ({
+  type: RECEIVE_ALL_MOVIES,
+  movies
+})
 
 const receiveMovie = movie => ({
   type: RECEIVE_MOVIE,
   movie
 })
+
+export const requestAllMovies = () => dispatch => {
+  return APIUtil.fetchAllMovies()
+  .then(movies => dispatch(receiveAllMovies(movies)))
+}
 
 export const requestMovie = id => dispatch => {
   return APIUtil.fetchMovie(id)
--- frontend/reducers/movies_reducer.js
+++ frontend/reducers/movies_reducer.js
@@ -1,7 +1,17 @@
-import { RECEIVE_MOVIE } from '../actions/movie_actions';
+import { RECEIVE_ALL_MOVIES, RECEIVE_MOVIE } from '../actions/movie_actions';
 
 const movieReducer = (state = {}, action) => {
-
+  Object.freeze(state);
+  const nextState = Object.assign({}, state);
+  switch (action.type){
+    case RECEIVE_ALL_MOVIES:
+      return Object.assign({}, action.movies, state)
+    case RECEIVE_MOVIE:
+      nextState[action.movie.id] = action.movie;
+      return nextState;
+    default:
+      return state;
+  }
 }
 
 export default movieReducer;
