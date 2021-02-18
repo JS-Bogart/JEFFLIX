54fc0fcf5ccd182e35e03aba90f78237ec10924e
Bogey <jeffreysbogart@gmail.com>
2021-02-18 11:32:09 -0800

    Add mute button to billboard

--- frontend/components/browse/billboard.jsx
+++ frontend/components/browse/billboard.jsx
@@ -3,23 +3,41 @@ import React from 'react';
 class Billboard extends React.Component{
   constructor(props){
     super(props);
-
+    this.state = {
+      muteButton: "muted"
+    }
+    this.billboardRef = React.createRef();
+    this.unMute = this.unMute.bind(this);
   }
 
-  // unMute(){
-
-  // }
+  unMute(){
+    this.billboardRef.current.muted = !this.billboardRef.current.muted;
+    if (this.state.muteButton === "unmuted") {
+      this.setState({ muteButton: "muted" });
+    } else {
+      this.setState({ muteButton: "unmuted" });
+    }
+  }
 
   render(){
+    let muteButton;
+    if (this.state.muteButton === "unmuted"){
+      muteButton = <p>&#x1f50a;</p>
+    } else {
+      muteButton = <p>&#x1f507;</p>
+    }
     return(
       <div className="billboard">
         <img src={window.flashgordonimg} alt="flashgordonimg" />
         <video
-          // ref={billboardRef}
+          ref={this.billboardRef}
           src={window.flashgordon}
           autoPlay={true}
           muted />
         <p>A football player and his friends travel to the planet Mongo and find themselves fighting the tyranny of Ming the Merciless to save Earth.</p>
+        <div className="billboard-mute-btn" onClick={this.unMute}>
+          {muteButton}
+        </div>
       </div>
     )
   }
