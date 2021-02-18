c86d2c73ae3401d912c7311586118f4873fbd442
Bogey <jeffreysbogart@gmail.com>
2021-02-17 21:10:52 -0800

    Add styling to carousel

--- frontend/components/browse/carousels/arrow.jsx
+++ frontend/components/browse/carousels/arrow.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 const Arrow = ({ direction, clickFunction, glyph }) => (
   <div
-    className={`slide-arrow ${direction}`}
+    className={`slide-arrow-${direction}`}
     onClick={clickFunction}>
-    { glyph}
+    {glyph}
   </div>
 );
 
