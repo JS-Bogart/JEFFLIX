b64eb27ccb4315a20de55f974a0d4ab53c34ca20
Bogey <jeffreysbogart@gmail.com>
2021-04-19 00:20:35 -0700

    Add video controls to watch page

--- frontend/components/watch/watch.jsx
+++ frontend/components/watch/watch.jsx
@@ -26,9 +26,13 @@ class Watch extends React.Component {
       })
       this.setState({
         movie: <video
+          id="video"
+          controls
+          preload="metadata"
           src={currentMovie.videoUrl}
-          autoPlay={true}
-          muted
+          type="video/mp4"
+          // autoPlay={true}
+          // muted
         />
       });
     }
@@ -44,18 +48,99 @@ class Watch extends React.Component {
     if (prevProps.movies !== this.props.movies){
       this.setState({
         movie: <video
+          id="video"
+          controls
+          preload="metadata"
           src={currentMovie.videoUrl}
-          autoPlay={true}
-          muted
+          type="video/mp4"
+          // autoPlay={true}
+          // muted
         />
+      });
+    }
+    if (this.state.movie) {
+      const videoContainer = document.getElementById('videoContainer');
+      this.video = document.getElementById('video');
+      const video = document.getElementById('video');
+      video.controls = false;
+      const progress = document.getElementById('progress');
+      const progressBar = document.getElementById('progress-bar');
+      const fullscreen = document.getElementById('fs');
+
+      const isFullScreen = function () {
+        return !!(document.fullscreen || document.webkitIsFullScreen || document.mozFullScreen || document.msFullscreenElement || document.fullscreenElement);
+      }
+
+      const setFullscreenData = function (state) {
+        videoContainer.setAttribute('data-fullscreen', !!state);
+      }
+
+      const handleFullscreen = function () {
+        if (isFullScreen()) {
+          if (document.exitFullscreen) document.exitFullscreen();
+          else if (document.mozCancelFullScreen) document.mozCancelFullScreen();
+          else if (document.webkitCancelFullScreen) document.webkitCancelFullScreen();
+          else if (document.msExitFullscreen) document.msExitFullscreen();
+          setFullscreenData(false);
+        }
+        else {
+          if (videoContainer.requestFullscreen) videoContainer.requestFullscreen();
+          else if (videoContainer.mozRequestFullScreen) videoContainer.mozRequestFullScreen();
+          else if (videoContainer.webkitRequestFullScreen) videoContainer.webkitRequestFullScreen();
+          else if (videoContainer.msRequestFullscreen) videoContainer.msRequestFullscreen();
+          setFullscreenData(true);
+        }
+      }
+
+      video.addEventListener('loadedmetadata', function () {
+        progress.setAttribute('max', video.duration);
+      });
+      video.addEventListener('timeupdate', function () {
+        if (!progress.getAttribute('max')) progress.setAttribute('max', video.duration);
+        progress.value = video.currentTime;
+        progressBar.style.width = Math.floor((video.currentTime / video.duration) * 100) + '%';
+      });
+      progress.addEventListener('click', function (e) {
+        const pos = (e.pageX - this.offsetLeft) / this.offsetWidth;
+        video.currentTime = pos * video.duration;
+      });
+      fullscreen.addEventListener('click', function (e) {
+        handleFullscreen();
       });
     }
   }
 
+  pauseButton(){
+    if (this.video.paused) this.video.play();
+    else this.video.pause();
+  }
+
+  muteButton(){
+    this.video.muted = !this.video.muted;
+  }
+
   render() {
     return (
-      <div>
+      <div id="videoContainer">
         {this.state.movie}
+        <ul id="video-controls" className="controls">
+          <li>
+            <button
+              id="playpause"
+              type="button"
+              onClick={() => this.pauseButton()}
+            >
+              Play/Pause
+            </button>
+          </li>
+          <li className="progress">
+            <progress id="progress" value="0" min="0">
+              <span id="progress-bar"></span>
+            </progress>
+          </li>
+          <li><button id="mute" type="button" onClick={() => this.muteButton()}>Mute/Unmute</button></li>
+          <li><button id="fs" type="button">Fullscreen</button></li>
+        </ul>
       </div>
     )
   }
