db56d867d59e0601507b6598ecd0432729352f78
Bogey <jeffreysbogart@gmail.com>
2021-07-24 20:29:15 -0700

    Refactor carousel component with hooks

--- frontend/components/browse/carousels/carousel.jsx
+++ frontend/components/browse/carousels/carousel.jsx
@@ -1,67 +1,53 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Arrow from './arrow';
 import Slides from './slides';
 
-class Carousel extends React.Component{
-  constructor(props){
-    super(props);
-    this.state = {
-      currentSliderItem: 0
-    };
-    this.previousSlide = this.previousSlide.bind(this);
-    this.nextSlide = this.nextSlide.bind(this);
-  }
+const Carousel = (props) => {
 
-  previousSlide() {
+  const [currentSliderItem, setCurrentSliderItem] = useState(0);
+
+  const previousSlide = () => {
     const lastIndex = 2;
-    const { currentSliderItem } = this.state;
     const shouldResetIndex = currentSliderItem === 0;
     const index = shouldResetIndex ? lastIndex : currentSliderItem - 1;
 
-    this.setState({
-      currentSliderItem: index
-    });
+    setCurrentSliderItem(index);
   }
 
-  nextSlide() {
+  const nextSlide = () => {
     const lastIndex = 2;
-    const { currentSliderItem } = this.state;
     const shouldResetIndex = currentSliderItem === lastIndex;
     const index = shouldResetIndex ? 0 : currentSliderItem + 1;
 
-    this.setState({
-      currentSliderItem: index
-    });
+    setCurrentSliderItem(index);
   }
 
-  render(){
-    return(
-      <div className="carousel">
-        <Arrow
-          direction="left"
-          clickFunction={this.previousSlide}
-          currentSliderItem={this.state.currentSliderItem}
-          glyph="‹" />
+  return(
+    <div className="carousel">
+      <Arrow
+        direction="left"
+        clickFunction={previousSlide}
+        currentSliderItem={currentSliderItem}
+        glyph="‹" />
 
-        <Slides 
-          currentSliderItem={this.state.currentSliderItem}
-          movies={this.props.movies}
-          genre={this.props.genre}
-          addMovie={this.props.addMovie}
-          userId={this.props.userId}
-          myList={this.props.myList}
-          deleteMovie={this.props.deleteMovie}
-          handlePlayButton={this.props.handlePlayButton}
-        />
+      <Slides 
+        currentSliderItem={currentSliderItem}
+        movies={props.movies}
+        genre={props.genre}
+        addMovie={props.addMovie}
+        userId={props.userId}
+        myList={props.myList}
+        deleteMovie={props.deleteMovie}
+        handlePlayButton={props.handlePlayButton}
+      />
 
-        <Arrow
-          direction="right"
-          clickFunction={this.nextSlide}
-          currentSliderItem={this.state.currentSliderItem}
-          glyph="›" />
-      </div>
-    )
-  }
+      <Arrow
+        direction="right"
+        clickFunction={nextSlide}
+        currentSliderItem={currentSliderItem}
+        glyph="›" />
+    </div>
+  )
 }
 
 export default Carousel;
