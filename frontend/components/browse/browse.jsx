f6d9b5a49892918a12ac42da3a4353f2b0539ecf
Bogey <jeffreysbogart@gmail.com>
2021-07-26 21:40:06 -0700

    Refactor browse component using hooks

--- frontend/components/browse/browse.jsx
+++ frontend/components/browse/browse.jsx
@@ -1,40 +1,54 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Route } from 'react-router-dom';
 import NavbarContainer from '../navbar/navbar_container';
 import Carousel from './carousels/carousel';
 import Billboard from './billboard';
 
-class Browse extends React.Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      genresLoaded: false,
-      genres: []
-    };
-    this.getGenreList = this.getGenreList.bind(this);
-    this.handlePlayButton = this.handlePlayButton.bind(this);
-  }
+const Browse = (props) => {qqq
 
-  componentDidMount(){
-    if (this.props.movies.length < 2) {
-      this.props.requestAllMovies();
-    }
-    if (this.props.genres.length < 1) {
-      this.props.requestAllGenres();
-    }
-    this.props.getMyList(this.props.currentUser.id);
-  }
+  const [genres, setGenres] = useState([]);
+  const [genresLoaded, setGenresLoaded] = useState(false);
+  const [listLoaded, setListLoaded] = useState(false);
 
-  componentDidUpdate(prevProps) {
-    if (prevProps.genres !== this.props.genres || prevProps.movies !== this.props.movies) {
-      this.setState({
-        genresLoaded: false
-      });
-      this.loadGenres();
-    }
-  }
+  // componentDidMount(){
+  //   if (this.props.movies.length < 2) {
+  //     this.props.requestAllMovies();
+  //   }
+  //   if (this.props.genres.length < 1) {
+  //     this.props.requestAllGenres();
+  //   }
+  //   this.props.getMyList(this.props.currentUser.id);
+  // }
 
-  shuffle(array) {
+  // componentDidUpdate(prevProps) {
+  //   if (prevProps.genres !== this.props.genres || prevProps.movies !== this.props.movies) {
+  //     this.setState({
+  //       genresLoaded: false
+  //     });
+  //     this.loadGenres();
+  //   }
+  // }
+
+  useEffect(() => {
+    const moviesPresent = props.movies.length > 1;
+    const genresPresent = props.genres.length > 0;
+    if (props.movies.length < 2) {
+      props.requestAllMovies();
+    }
+    if (props.genres.length < 1) {
+      props.requestAllGenres();
+    }
+    if (!listLoaded) {
+      props.getMyList(props.currentUser.id);
+      setListLoaded(true);
+    }
+    if (moviesPresent && genresPresent) {
+      setGenresLoaded(false);
+      loadGenres();
+    }
+  }, [props.movies, props.genres])
+
+  const shuffle = (array) => {
     for (let i = array.length - 1; i > 0; i--) {
       const j = Math.floor(Math.random() * (i + 1));
       [array[i], array[j]] = [array[j], array[i]];
@@ -42,36 +56,33 @@ class Browse extends React.Component {
     return (array)
   }
 
-  loadGenres() {
-    if (!this.state.genresLoaded && this.props.movies.length > 1) {
-      const genres = this.shuffle(this.props.genres);
-      this.setState({ 
-        genresLoaded: true, 
-        genres: genres
-      });
+  const loadGenres = () => {
+    if (!genresLoaded && props.movies.length > 1) {
+      const shuffledGenres = shuffle(props.genres);
+      setGenres(shuffledGenres);
+      setGenresLoaded(true);
     }
   }
 
-  handlePlayButton(movie) {
-    this.props.history.push(`/watch/${movie}`);
+  const handlePlayButton = (movie) => {
+    props.history.push(`/watch/${movie}`);
   }
 
-  getGenres() {
-    const genres = this.state.genres;
-    if (this.props.movies.length > 0) {
+  const getGenres = () => {
+    if (props.movies.length > 0) {
       return (
         <div className="browse-genre-rows">
           {genres.map((genre, index) => (
             <div className="browse-row" key={index}>
               <h2>{genre.genre}</h2>
               <Carousel
-                movies={this.props.movies}
+                movies={props.movies}
                 genre={genre}
-                addMovie={this.props.addMovie}
-                userId={this.props.currentUser.id}
-                myList={this.props.myList}
-                deleteMovie={this.props.deleteMovie}
-                handlePlayButton={this.handlePlayButton}
+                addMovie={props.addMovie}
+                userId={props.currentUser.id}
+                myList={props.myList}
+                deleteMovie={props.deleteMovie}
+                handlePlayButton={handlePlayButton}
               />
             </div>
           ))}
@@ -80,9 +91,8 @@ class Browse extends React.Component {
     }
   }
 
-  getGenreList(){
-    const genres = this.state.genres;
-    if (this.state.genres.length) {
+  const getGenreList = () => {
+    if (genres.length) {
       return(
         <ul className="browse-genre-list">
           {genres.map((genre, index) => (
@@ -95,30 +105,28 @@ class Browse extends React.Component {
     }
   }
 
-  render() {
-    return (
-      <div className="browse">
-        <Route component={NavbarContainer} />
-        <div className="browse-body">
-          <div className="browse-title">
-            <h1>Movies</h1>
-            <div className="genre-menu">
-              <div className="genre-menu-title">
-                <p>Genres</p>
-                <p>&#x25BE;</p>
-              </div>
-              <div className="genre-dropdown">
-                {this.getGenreList()}
-              </div>
+  return (
+    <div className="browse">
+      <Route component={NavbarContainer} />
+      <div className="browse-body">
+        <div className="browse-title">
+          <h1>Movies</h1>
+          <div className="genre-menu">
+            <div className="genre-menu-title">
+              <p>Genres</p>
+              <p>&#x25BE;</p>
+            </div>
+            <div className="genre-dropdown">
+              {getGenreList()}
             </div>
           </div>
-          <Billboard/>
-          {this.getGenres()}
-          <footer></footer>
         </div>
+        <Billboard/>
+        {getGenres()}
+        <footer></footer>
       </div>
-    );
-  }
+    </div>
+  );
 };
 
 export default Browse;
